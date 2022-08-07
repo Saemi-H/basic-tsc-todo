285f110c5128cde6b60480c8b7a228421c645013
Saemi H <sstyle940104@gmail.com>
2022-08-07 11:00:37 +0900

    feat: created tasks array and checkbox status in index

--- src/index.ts
+++ src/index.ts
@@ -13,27 +13,34 @@ const list = document.querySelector<HTMLUListElement>('#list');
 const form = document.getElementById('new-task-form') as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
 
-// add click event listeners
+// create task array
+const tasks: Task[] = [];
+
 form?.addEventListener('submit', e => {
   e.preventDefault();
 
   //input?.value => ? : null 일 수도 있기 때문에 null 이 아닐때만
   if(input?.value == "" || input?.value == null) return
-  // add task obj
   const newTask: Task = {
     id: uuidV4(),
     title: input?.value,
     completed: false,
     createdAt: new Date()
   }
+  tasks.push(newTask);
   addListItem(newTask);
+  input.value = "";
 })
 
 function addListItem(task: Task){
   const item = document.createElement("li");
   const label = document.createElement("label");
   const checkbox = document.createElement("input");
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked;
+  })
   checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
   label.append(checkbox, task.title)
   item.append(label)
   list?.append(item)
