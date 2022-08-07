b2db1545a17eee6364701116f188093dad960f39
Saemi H <sstyle940104@gmail.com>
2022-08-07 10:17:42 +0900

    feat: add eventclick listener in index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-// import {v4 as uuidV4} from "uuid";
-
-// uuidV4()
+import {v4 as uuidV4} from "uuid";
 
 // generics : <>
 // || as
 const list = document.querySelector<HTMLUListElement>('#list');
 const form = document.getElementById('#new-task-form') as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>('#lnew-task-title');
+
+// add click event listeners
+form?.addEventListener('submit', e => {
+  e.preventDefault();
+
+  //input?.value => ? : null 일 수도 있기 때문에 null 이 아닐때만
+  if(input?.value === "" || input?.value === null) return
+  // add task obj
+  const task = {
+    id: uuidV4(),
+    title: input?.value,
+    completed: false,
+    createdAt: new Date()
+  }
+})
