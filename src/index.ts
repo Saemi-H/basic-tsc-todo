4c3b17b403f69295fac909efabcf6a6b6c3dc555
Saemi H <sstyle940104@gmail.com>
2022-08-07 11:24:30 +0900

    feat: completed

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,59 @@
 import {v4 as uuidV4} from "uuid";
 
 type Task = {
-  id: string, 
-  title: string, 
-  completed: boolean, 
+  id: string 
+  title: string
+  completed: boolean
   createdAt: Date
 }
 
 // generics : <>
 // || as
-const list = document.querySelector<HTMLUListElement>('#list');
-const form = document.getElementById('new-task-form') as HTMLFormElement | null;
-const input = document.querySelector<HTMLInputElement>('#new-task-title');
-
-// create task array
-const tasks: Task[] = [];
+const list = document.querySelector<HTMLUListElement>('#list')
+const form = document.getElementById('new-task-form') as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>('#new-task-title')
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem);
 
 form?.addEventListener('submit', e => {
   e.preventDefault();
 
   //input?.value => ? : null 일 수도 있기 때문에 null 이 아닐때만
   if(input?.value == "" || input?.value == null) return
+  
   const newTask: Task = {
     id: uuidV4(),
     title: input?.value,
     completed: false,
     createdAt: new Date()
   }
-  tasks.push(newTask);
+  tasks.push(newTask)
   saveTasks();
-  addListItem(newTask);
-  input.value = "";
+  addListItem(newTask)
+  input.value = ""
 })
 
 function addListItem(task: Task){
-  const item = document.createElement("li");
-  const label = document.createElement("label");
-  const checkbox = document.createElement("input");
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
   checkbox.addEventListener("change", () => {
-    task.completed = checkbox.checked;
+    task.completed = checkbox.checked
     saveTasks();
   })
-  checkbox.type = "checkbox";
-  checkbox.checked = task.completed;
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
   label.append(checkbox, task.title)
   item.append(label)
   list?.append(item)
 }
 
-// saving at the local storage
 function saveTasks(){
-  localStorage.saveItem('TASKS', JSON.stringify(tasks));
+  localStorage.setItem('TASKS', JSON.stringify(tasks))
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS")
+  if(taskJSON == null) return []
+  return JSON.parse(taskJSON);
 }
